1c5d334b63f64253b8ab60320278869c93ecfc1a
hahadaniel <513381618@qq.com>
2016-01-17 18:12:19 +0800

    Finish building the database.

---
+++ app/src/main/java/com/scut/zj/clockin/ClockInDB.java
@@ -0,0 +1,64 @@
+package com.scut.zj.clockin;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by zj on 2016/1/17.
+ */
+public class ClockInDB extends SQLiteOpenHelper{
+
+    public static final String ID = "id";
+    public static final String RECORD_ID = "record_id";
+
+    //table clock_in_item
+    public static final String NAME = "name";
+    public static final String REMINDING_TIME = "reminding_time";
+    public static final String PRESET_HOUR = "preset_hour";
+
+    //table clock_in_record
+    public static final String CLOCK_DATE = "clock_date";
+    public static final String RATE = "rate";
+    public static final String CLOCK_HOUR = "clock_hour";
+
+    //table clock_in_item_record
+    public static final String ITEM_ID = "item_id";
+
+    //table day_evaluation_record
+    public static final String EVALUATION = "evaluation";
+
+    public ClockInDB(Context context) {
+        super(context, "clock_in.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE clock_in_item("
+                +ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
+                +NAME+" TEXT NOT NULL,"
+                +REMINDING_TIME+" TEXT,"
+                +PRESET_HOUR+" INTEGER);");
+        db.execSQL("CREATE TABLE clock_in_record("
+                +ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
+                +RATE+" INTEGER NOT NULL,"
+                +CLOCK_DATE+" TEXT NOT NULL,"
+                +CLOCK_HOUR+" INTEGER);");
+        db.execSQL("CREATE TABLE clock_in_item_record("
+                +ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
+                +ITEM_ID+" INTEGER NOT NULL,"
+                +RECORD_ID+" INTEGER NOT NULL"
+                +"FOREIGN KEY("+ITEM_ID+") REFERENCES clock_in_item("+ID+")"
+                +"FOREIGN KEY("+RECORD_ID+") REFERENCES clock_in_record("+ID+"));");
+        db.execSQL("CREATE TABLE day_evaluation_record("
+                +ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
+                +EVALUATION+" TEXT NOT NULL"
+                +RECORD_ID+" INTEGER NOT NULL"
+                +"FOREIGN KEY("+RECORD_ID+") REFERENCES clock_in_record("+ID+"));");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
